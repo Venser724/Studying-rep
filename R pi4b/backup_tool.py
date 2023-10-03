b6fb0493ff0ef4e7d8c21124a158bd18e5e2a59c
Andrey Venser <marked73@gmail.com>
2023-10-03 22:16:03 +0300

    adding TODO list

--- R pi4b/backup_tool.py
+++ R pi4b/backup_tool.py
@@ -1,10 +1,17 @@
-import os
-import shutil
-#import usb-lib XZ
-source_path = 'C:/Users/Venser/Desktop/Работа/Новое/Фоспренил 50/' #write smth to find path
-#print(os.listdir(source_path))
-dest_path = 'C:/Users/Venser/Desktop/123/copy_test' #write smth to find path
+import os       # stdlib
+import shutil   # stdlib
+import usb      # pyusb>=1.2.1
+# g: python venv freeze requirements
+# python -m pip install -r requirements.txt
 
-os.makedirs(os.path.dirname('C:/Users/Venser/Desktop/123/copy_test'), exist_ok=True)
-shutil.copytree(source_path, dest_path)
+def find_source_path(source_path):
+ from usb.core
+
+source_path = '' 
+#def find_dest_path(dest_path):
+
+dest_path_1 = ''
+
+os.makedirs(os.path.dirname(''), exist_ok=True)
+shutil.copytree(source_path, dest_path_1)
 print('Complete')
