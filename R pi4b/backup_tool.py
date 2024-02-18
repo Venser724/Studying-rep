5c95ead714ee058d3f6a65e28fadac2808c8de23
Andrey Venser <marked73@gmail.com>
2024-02-18 11:52:58 +0300

    starting Kotlin learning

--- R pi4b/backup_tool.py
+++ R pi4b/backup_tool.py
@@ -1,3 +1,4 @@
+from importlib.util import source_hash
 import os       # stdlib
 import shutil   # stdlib
 import usb      # pyusb>=1.2.1
@@ -5,16 +6,17 @@ import usb      # pyusb>=1.2.1
 # python -m pip install -r requirements.txt
 
 def find_source_path(source_path):
+    devices = usb.core.find(find_all=True, bDeviceClass = 8)
+    print(f'Devices found: ", +, "{len(devices)}')
+    return(source_path)
+print(source_hash)
+#source_path = '' 
+#def find_dest_path(dest_path):
   #from usb.core
-  pass
+  #return None
 
-source_path = '' 
-def find_dest_path(dest_path):
-  #from usb.core
-  return None
+#dest_path_1 = ''
 
-dest_path_1 = ''
-
-os.makedirs(os.path.dirname(''), exist_ok=True)
-shutil.copytree(source_path, dest_path_1)
-print('Complete')
+#os.makedirs(os.path.dirname(''), exist_ok=True)
+#shutil.copytree(source_path, dest_path_1)
+#print('Complete')
